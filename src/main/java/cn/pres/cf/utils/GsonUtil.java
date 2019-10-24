3bd15dfee72c31b1591f5df95d39561b776c2acf
chenfan <chenfan@skytech.cn>
2019-10-24 13:25:50 +0800

    optimize:去掉多余maven依赖

--- src/main/java/cn/pres/cf/utils/GsonUtil.java
+++ src/main/java/cn/pres/cf/utils/GsonUtil.java
@@ -18,46 +18,54 @@ import java.util.List;
  **/
 @Slf4j
 public class GsonUtil {
-  private  static  Gson gson = new GsonBuilder()
-          .serializeNulls()
-          .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
-          .registerTypeAdapter(LocalDateTime.class,new LocalDateTimeTypeAdapter())
-          .setDateFormat("yyyy-MM-dd hh:mm:ss")
-          .create();
-  static {
 
-  }
+    private GsonUtil() {
+    }
+
+    private static Gson gson = new GsonBuilder()
+            .serializeNulls()
+            .registerTypeAdapter(LocalDate.class, new LocalDateTypeAdapter())
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeTypeAdapter())
+            .setDateFormat("yyyy-MM-dd hh:mm:ss")
+            .create();
+
+    static {
+
+    }
+
     /**
      * 对象转json
-     * @author dora 
-     * @return   
+     *
+     * @return
+     * @author dora
      * @date 2019/10/23
-    **/
-    public static String toJson( Object src){
+     **/
+    public static String toJson(Object src) {
         return gson.toJson(src);
     }
 
     /**
      * json 转对象
-     * @author dora 
-     * @return   
+     *
+     * @return
+     * @author dora
      * @date 2019/10/23
-    **/
-    public static <T> T fromJson( String json, @NotNull Class<T> classOfT ){
-        return gson.fromJson(json,classOfT);
+     **/
+    public static <T> T fromJson(String json, @NotNull Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
     }
 
     /**
      * 文本转list
-     * @author dora 
-     * @return   
+     *
+     * @return
+     * @author dora
      * @date 2019/10/23
-    **/
-    public static <T> List<T> textToList(String text,@NotNull Class<T> classOfT){
-        return  gson.fromJson(text,new TypeToken<List<T>>(){}.getType());
+     **/
+    public static <T> List<T> textToList(String text, @NotNull Class<T> classOfT) {
+        return gson.fromJson(text, new TypeToken<List<T>>() {
+        }.getType());
     }
-
-
 
 
 }
